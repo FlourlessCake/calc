cb447b74ca9bf68a882ddf0dc16b999a00b7157a
x0xl0ma <k0tjaka@yandex.ru>
2020-05-14 16:14:24 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React, { useState } from "react";
-import data from "./depcalc.json";
+import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import AccessibleForwardSharpIcon from "@material-ui/icons/AccessibleForwardSharp";
 import Calculator from "./Components/Calculator.js";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <>
-      <nav className="navbar navbar-light bg-light">
-        <div className="navbar-brand">Choose your rabstvo jebanoe</div>
-      </nav>
-      <div className="container">
-        <Calculator />
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <AccessibleForwardSharpIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Rabstvo jebanoe
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
+      <Calculator />
     </>
   );
 }
--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,88 +1,156 @@
 import React, { useState } from "react";
-import Display from "./Display";
+import { Formik, Form, useField } from "formik";
+import {
+  TextField,
+  Button,
+  Radio,
+  FormControlLabel,
+  Container,
+} from "@material-ui/core";
+import * as yup from "yup";
 import data from "../depcalc.json";
 
-import "../styles/Calculator.css";
-import "react-input-range/lib/css/index.css";
+const MyRadio = ({ label, ...props }) => {
+  const [field] = useField(props);
+  return <FormControlLabel {...field} control={<Radio />} label={label} />;
+};
 
-const Calculator = () => {
+const MyTextField = ({ placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : "";
+  return (
+    <TextField
+      placeholder={placeholder}
+      {...field}
+      helperText={errorText}
+      error={!!errorText}
+      style={{ marginBottom: 10 }}
+    />
+  );
+};
+
+const helper = (name, value) => {
   const { deposits } = data;
-  const finder = (depoType) => deposits.find((d) => d.code === depoType);
+  const depo = deposits.find((d) => d.code === name);
   const nearestBelow = (input, lookup) =>
     lookup.reduce((prev, curr) => (input >= curr ? curr : prev));
+  const maxCurr = nearestBelow(
+    value,
+    depo.param.map((d) => d.period_from)
+  );
+  const investPeriod = depo.param.find((d) => d.period_from === maxCurr);
+  const clientRate = nearestBelow(value, investPeriod.summs_and_rate);
+  return { investPeriod, clientRate };
+};
 
-  const [amountValue, setAmountValue] = useState(0);
-  const [daysValue, setDaysValue] = useState(null);
-  const [type, setType] = useState("unic");
+const validationSchema = yup.object({
+  name: yup.string().required(),
+  term: yup.number().when("name", (name, schema, { value }) => {
+    const { investPeriod } = helper(name, value);
+    return schema.required().min(investPeriod.period_from);
+  }),
+  amount: yup.number().when("name", (name, schema, { value }) => {
+    const { clientRate } = helper(name, value);
+    return schema.required().min(clientRate.summ_from);
+  }),
+});
 
-  const helpers = (depoType) => {
-    const deposit = finder(depoType)
-    const maxCurrentPeriodOfDeposit = nearestBelow(
-      daysValue,
-      deposit.param.map((d) => d.period_from)
-    );
-    const investPeriod = deposit.param.find(
-      (d) => d.period_from === maxCurrentPeriodOfDeposit
-    );
-    const clientRate = nearestBelow(amountValue, investPeriod.summs_and_rate);
+const Calculator = () => {
+  const [dData, setData] = useState({
+    name: "",
+    term: "",
+    amount: "",
+  });
 
-    return {investPeriod, clientRate}
-  }
-  const {investPeriod, clientRate} = helpers(type)
+  const handleRate = (dData) => {
+    if (dData.name === "" || dData.amount === "") {
+      return;
+    }
+    const { clientRate } = helper(dData.name, dData.amount);
+    return clientRate.rate;
+  };
+  const rate = handleRate(dData);
 
-  console.log(investPeriod)
-
-  const handleDepo = (value) => {
-    setType(value);
+  const calculateDivs = (dData) => {
+    if (dData.term === "" || dData.amount === "") {
+      return;
+    }
+    const dividends = ((dData.amount * rate) / 36500) * dData.term;
+    return <p>{Math.round(dividends)} rub</p>;
   };
 
   return (
-    <div className="container">
-      {deposits.map((d) => (
-        <div key={d.code} className="form-check form-check-inline">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id={d.code}
-            value="option1"
-            onChange={() => handleDepo(d.code)}
-          />
-          <label className="form-check-label" htmlFor={d.code}>
-            {d.name}
-          </label>
+    <Container maxWidth="md">
+      <Formik
+        validateOnChange={true}
+        initialValues={{
+          name: "",
+          term: "",
+          amount: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          setTimeout(() => {
+            setData({ ...data })
+            setSubmitting(false);
+          }, 2000)
+          
+        }}
+      >
+        {({ values, isSubmitting }) => (
+          <Form>
+            <div>Rates:</div>
+            <MyRadio
+              name="name"
+              type="radio"
+              value="unic"
+              label="Универсальный"
+            />
+            <MyRadio
+              name="name"
+              type="radio"
+              value="standart"
+              label="Стандартный"
+            />
+            <MyRadio
+              name="name"
+              type="radio"
+              value="replenish"
+              label="Пополняемый"
+            />
+            <div>
+              {values.name && (
+                <MyTextField placeholder="Сумма вклада" name="amount" />
+              )}
+            </div>
+            <div>
+              <div>
+                {values.name && values.amount && (
+                  <MyTextField placeholder="Срок вклада" name="term" />
+                )}
+              </div>
+              <Button
+                disabled={isSubmitting || !values.name}
+                color="secondary"
+                variant="contained"
+                type="submit"
+              >
+                submit
+              </Button>
+            </div>
+            {/* <pre>{JSON.stringify(values, null, 2)}</pre>
+            <pre>{JSON.stringify(errors, null, 2)}</pre> */}
+          </Form>
+        )}
+      </Formik>
+      {rate && (
+        <div style={{display: "flex", justifyContent: "space-between", fontSize: 24, fontWeight: "bold"}}>
+            <div>Deposit Rate: {rate}%</div>
+            <div>Your adds: {calculateDivs(dData)}</div>
         </div>
-      ))}
-
-      <h4>I want to invest {amountValue} rub</h4>
-      <div className="form-group">
-        <label htmlFor="amount">Amount of deposit</label>
-        <input
-          type="number"
-          className="form-control"
-          id="amount"
-          onChange={(e) => setAmountValue(e.target.value)}
-          min={clientRate.summ_from}
-        />
-        <small id="amount" className="form-text text-muted">
-          Min amount of deposit on this Rate is {clientRate.summ_from}
-        </small>
-      </div>
-      <div className="form-group">
-        <label htmlFor="term">Term of deposit</label>
-        <input
-          type="number"
-          className="form-control"
-          id="term"
-          onChange={(e) => setDaysValue(e.target.value)}
-          min={investPeriod.period_from}
-        />
-        <small id="term" className="form-text text-muted">
-          Min invest period is {investPeriod.period_from}
-        </small>
-      </div>
-      <Display days={daysValue} amount={amountValue} rate={clientRate.rate} />
-    </div>
+      )}
+    </Container>
   );
 };
 
